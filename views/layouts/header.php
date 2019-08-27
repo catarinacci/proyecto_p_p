7497c9bb1a1d8f4aedf821b57b78173494296d6e
NicoDanelon <nicodanelon@outlook.com>
2019-08-26 22:55:41 -0300

    index admin

---
+++ views/layouts/header.php
@@ -0,0 +1,43 @@
+<header class="">
+
+<div class="row p-2 ">
+
+    <div class="col-auto logo">
+        <a href="../inicio/indexmenu.php"><img src="../../assets/img/logo.png" width="45" height="45" alt=""></a>
+        <a href="../inicio/indexmenu.php">TIGER GYM</a>
+    </div>
+
+
+    <div class="col-8 menu d-none d-md-block">
+        <div class="row">
+            <div class="col-3 text-center">
+                <a href="../inicio/indexmenu.php">Inicio</a>
+            </div>
+
+            <div class="col-3 text-center">
+                <a href="../cuenta/cuenta.php">Cuenta</a>
+            </div>
+
+            <div class="col-3 text-center">
+                <a href="../actividades/actividades.php">Actividades</a>
+            </div>
+
+            <div class="col-3 text-center">
+                <a href="../gestion/gestion.php">Gestion</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col menudesplegable d-flex justify-content-end ml-auto">
+        <a href="#" class="btn-menu d-md-none" id="btn-menu">
+            <i class="icon-menu"></i>
+        </a>
+    </div>
+
+    <nav class="col-1 login  d-none d-md-block">
+        <a href=""><i class="icon-user"></i></a>
+    </nav>
+
+
+</div>
+</header>
---
+++ views/usuario/registro.php
@@ -0,0 +1,31 @@
+<?php
+
+include "includes/header.html"
+
+?>
+
+<h1>Registrarse</h1>
+
+<form action="index.php?controller=usuario&action=save" method="POST">
+
+    <label for="nombre">Nombre</label>
+    <input type="text" name="Nombre" required/>
+
+    <label for="apellido">Apellido</label>
+    <input type="text" name="apellido" required/>
+
+    <label for="email">Email</label>
+    <input type="text" name="email" required/>
+
+    <label for="password">Contrasena</label>
+    <input type="text" name="password" required/>
+
+    <label for="telefono">Telefono</label>
+    <input type="text" name="telefono" required/>
+
+    <label for="direccion">Direccion</label>
+    <input type="text" name="direccion" required/>
+
+    <input tupe="submit" value="Registrarse"/>
+
+</form>
